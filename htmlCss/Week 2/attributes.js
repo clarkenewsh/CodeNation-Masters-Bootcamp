f9b5ec3281553b35104bf61eadd3a5a91d2894d6
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-01-20 10:21:33 +0000

    week 2 file name change

---
+++ htmlCss/Week 2/attributes.js
@@ -0,0 +1,5 @@
+let shoppingList = [["bread", "crisps", "pizza", "coffee"],
+                    ["cheese", "water", "chocolate"],
+                    ["noodles", "apples", "bananas"]];
+
+console.log(shoppingList[1][2]);
