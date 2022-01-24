f812ee1b6e830328b6cee14e34455ca9cfad485e
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-01-24 16:34:06 +0000

    folder restructure

--- HTML & CSS/Week 2/attributes.js
+++
@@ -1,5 +0,0 @@
-let shoppingList = [["bread", "crisps", "pizza", "coffee"],
-                    ["cheese", "water", "chocolate"],
-                    ["noodles", "apples", "bananas"]];
-
-console.log(shoppingList[1][2]);
