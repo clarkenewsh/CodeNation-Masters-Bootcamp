98723e76c960dcc78837211d447cd61f5c075565
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-01-26 09:46:29 +0000

    challenge 5 & 7 complete with working examples

--- jsDom/Week3/jsdom1/5 - textContent and innerHTML/app.js
+++ jsDom/Week3/jsdom1/5 - textContent and innerHTML/app.js
@@ -1,12 +1,33 @@
-const heading = document.getElementById("placeholder")
-const input = document.getElementById("input")
-const submit = document.getElementById("submit")
-// const list = document.querySelectorAll("li");
-let list = document.getElementsByTagName("ul")[0];
+const placeholder = document.getElementById("placeholder");
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+const list = document.getElementById("list");
 
-submit.addEventListener("click",() => {
-    const item = document.createElement("li")
-    item.textContent = input.value
-    list.appendChild(item)
-    input.value = ""
+
+// submit.addEventListener("click", () =>{
+//     placeholder.style.color ="goldenrod";
+//     placeholder.textContent = input.value;
+// })
+
+// Using template literals 
+submit.addEventListener("click", () =>{
+    placeholder.style.color ="goldenrod";
+    // adds text content within placeholder html - does not create html
+    // placeholder.textContent = `<li>${input.value}</li>`;
+    //replaces all list items
+    list.innerHTML = `<li>${input.value}</li>`;
 })
+
+// Challenge 5
+// const heading = document.getElementById("placeholder")
+// const input = document.getElementById("input")
+// const submit = document.getElementById("submit")
+// // const list = document.querySelectorAll("li");
+// let list = document.getElementsByTagName("ul")[0];
+
+// submit.addEventListener("click",() => {
+//     const item = document.createElement("li")
+//     item.textContent = input.value
+//     list.appendChild(item)
+//     input.value = ""
+// })
---
+++ jsDom/Week3/jsdom1/7 - Styling Elements with JS/app.js
@@ -0,0 +1,13 @@
+// Styling elements - Toggle show/hide
+const button= document.getElementById("submit");
+const image = document.getElementById("cat");
+button.addEventListener("click", () => {
+    if(image.style.display == "none") {
+        image.style.display = "block";
+        button.textContent = "hide";
+
+    } else {
+        image.style.display = "none";
+        button.textContent = "show";
+    }
+});
