4206e04c60612815cf8e043938e4e08e765c9c1f
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-01-27 14:13:58 +0000

    styles and sounds folders added & html markup created

---
+++ jsDom/Week3/drum-kit-challenge/drumKit.js
@@ -0,0 +1,8 @@
+// TASK LIST
+//     1 - Create html placeholders divs with drum key codes.
+//     2 - Get element by id for each drum 
+//     2 - Create event listener function with switch statement to play a certain sound when a key code is pressed that relates to the div with that assigned key code data
+//     3 - Create functions to play each drum sound to then call the function in the event listener on keypress code
+//     4 - html audio tags - research how to use these 
+//     5 - 
+
