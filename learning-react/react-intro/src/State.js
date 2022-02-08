4c03c7233ddc361ae599f8fc40bd7521ee77bb81
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-02-08 12:44:35 +0000

    passing handleClick function as props to Person component then using OnClick annom function to pass the age property to handleClick function using params

--- learning-react/react-intro/src/State.js
+++ learning-react/react-intro/src/State.js
@@ -13,6 +13,10 @@ const State = () => {
     const incrementCount = () => {
         setCount(count + 1);
     }
+    // using parameter age to catch the props.age property from - onClick={() => props.clickMe(props.age)
+    const handleClick = (age) => {
+        alert(age);
+    }
 
     return (
         <div>
@@ -20,10 +24,11 @@ const State = () => {
             {/* <button onClick={() => setCount(count + 1)}></button> */}
             <button onClick={incrementCount}>Increase count</button>
             <Person 
-                name={person[0].name}
+                name={person[0].name} 
                 age={person[0].age}
+                clickMe={handleClick}
              />
-            {/* // get entire person */}
+             {/* // get entire person */}
              {/* <Person person={person[0]} />
              <Person person={person[1]} /> */}
         </div>
@@ -35,10 +40,13 @@ const Person = (props) => {
         <div>
             {/* <h1>{props.person.name}</h1>
             <h2>{props.person.age}</h2> */}
-            <h1>{props.name}</h1>
-            <h2>{props.age}</h2>
+            <h1 onClick={props.clickMe}>{props.name}</h1>
+            {/* annom function to pass a parameter to the handleClick function - in this case age */}
+            <h2 onClick={() => props.clickMe(props.age)}>{props.age}</h2>
         </div>
     )
 }
+
+
 
 export default State;
