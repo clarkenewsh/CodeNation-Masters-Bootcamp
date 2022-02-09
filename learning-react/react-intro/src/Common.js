e6f054855ae8a8d54eed645aa607efaa3f3fb462
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-02-09 12:55:18 +0000

    common js components - add to array, remove from array index, updating state, onChange evnt handler

--- learning-react/react-intro/src/Common.js
+++ learning-react/react-intro/src/Common.js
@@ -5,6 +5,8 @@ const Common = () => {
 
     const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
 
+    const [inputText, setInputText] = useState('');
+
     // function to push numbers on to array using state
     const addHandler = () => {
         // spread over numbers, copy array and store in new array storedNums
@@ -15,13 +17,36 @@ const Common = () => {
         setNumbers(storedNums);
     }
 
+    // remove number from specific index in the array when clicking on it
+    const removeHandler = (index) => {
+         // spread over numbers, copy array and store in new array storedNums
+        let storedNums = [...numbers];
+
+        // splice from index in array clicked then remove, 1 number 
+        storedNums.splice(index, 1);
+        setNumbers(storedNums);
+    }
+
+    // onChnage handler to update state using the text typed in the input
+    const changeHandler = (e) => {
+        setInputText(e.target.value);
+    }
+
+
+
   return (
       <div>
           <h1>Common React components</h1>
-          {numbers.map((number, index) => {
-              return <h1 key={index}>{ number }</h1>
+            {numbers.map((number, index) => {
+            // using annom function to call removeHandler onclick, passing the index of that number of the array to the removeHandler function - must use annom when passing in 
+              return <h1 key={index} onClick={() => removeHandler(index)}>{ number }</h1>
           })}
           <button onClick={addHandler}>Add number to array</button>
+
+          {/* On change event - change value when input value changes */}
+          <input type='text' onChange={(e) => changeHandler(e)}></input>
+          {/* Output input value when typed in the input box */}
+          <h2>{inputText}</h2>
       </div>
   )
     
