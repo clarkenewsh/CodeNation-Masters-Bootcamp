4420dae4a192730f788231508bc210c7a9c0d7e8
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-02-14 14:07:06 +0000

    using asyn, await & fetch to get api endpoint including error handling

--- learning-react/advice-app/src/App.js
+++ learning-react/advice-app/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+
+  // state used to store fetch request
+  const [advice, setAdvice] = useState('');
+
+  // async, await fetch request - with error catching
+  const collect = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advie');
+      console.log(response);
+      if(response.status !== 200) {
+        // if error, throw and catch in catch block
+        throw new Error('oops');
+      }
+      // parse data into json
+      const data = await response.json();
+      setAdvice(data.slip);
+      // catch errors that occur in the try
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
-
+      <h1>Advice App - API Requests</h1>
+      <h2>advice: {advice.advice}</h2>
+      <button onClick={collect}>Collect data</button>
     </div>
   )
 }
